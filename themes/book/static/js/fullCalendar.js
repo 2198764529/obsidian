7ab8a635367fd51d075f31681243fc4938b7dade
2198764529 <2198764529@qq.com>
2023-10-26 11:08:24 +0800

    vault backup: 2023-10-26 11:08:24

---
+++ themes/book/static/js/fullCalendar.js
@@ -0,0 +1,24 @@
+
+function calendarRender(content){
+
+    document.addEventListener('DOMContentLoaded', function() {
+        // let content = "{{ $page := .Site.GetPage `/posts/日志.md` }}{{ $page.RawContent }}";
+    
+        var calendarEl = document.getElementById('calendar');
+        const jsonData = parseMarkdownToJSON(content);
+        console.log('jsonData',jsonData,content);
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth',
+          locale: 'zh-cn',
+    
+          events:jsonData,
+          headerToolbar: {
+            right: 'prev,next today', // 左侧按钮，包括前一个、后一个和今天
+      // center: 'title',        // 中间标题
+      left:"title"
+      // right: 'dayGridMonth,timeGridWeek,timeGridDay' // 右侧切换视图的按钮
+    },
+        });
+        calendar.render();
+      });
+}
