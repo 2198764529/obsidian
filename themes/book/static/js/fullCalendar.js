9943864ad830d304a0cdaa366af9ad2ea955885c
2198764529 <2198764529@qq.com>
2023-12-05 18:44:04 +0800

    vault backup: 2023-12-05 18:44:04

--- themes/book/static/js/fullCalendar.js
+++ themes/book/static/js/fullCalendar.js
@@ -1,7 +1,15 @@
 function calendarRender(content) {
   document.addEventListener("DOMContentLoaded", function () {
     // let content = "{{ $page := .Site.GetPage `/posts/日志.md` }}{{ $page.RawContent }}";
-
+    const specificDateTime = new Date(2012, 11, 2, 18, 23); // 月份是从 0 开始的，所以 11 表示12月
+    const year = specificDateTime.getFullYear();
+    const month = String(specificDateTime.getMonth() + 1).padStart(2, '0'); // 月份加1
+    const day = String(specificDateTime.getDate()).padStart(2, '0');
+    const hours = String(specificDateTime.getHours()).padStart(2, '0');
+    const minutes = String(specificDateTime.getMinutes()).padStart(2, '0');
+    
+    const dateTimeString = `⏰${year}年${month}月${day}日 ${hours}时${minutes}分`;
+    
     var calendarEl = document.getElementById("calendar");
     const jsonData = parseMarkdownToJSON(content);
     var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -11,14 +19,33 @@ function calendarRender(content) {
       locale: "zh-cn",
       events: jsonData,
       headerToolbar: {
-        right: "prev,next today", // 左侧按钮，包括前一个、后一个和今天
+        right: "", // 左侧按钮，包括前一个、后一个和今天
         // center: 'title',        // 中间标题
-        left: "title",
+        left: "today",
         // right: 'dayGridMonth,timeGridWeek,timeGridDay' // 右侧切换视图的按钮
+       
 
       },
-      titleFormat: { month: 'short', day: 'numeric' }
+      buttonText: {
+        today: dateTimeString, // 更改 "today" 按钮的标题
+        month: '月视图', // 更改 "month" 按钮的标题
+        week: '周视图', // 更改 "week" 按钮的标题
+        day: '日视图' // 更改 "day" 按钮的标题
+      },
+      titleFormat: { month: 'short', day: 'numeric' },
+      dayCellContent: function (arg) {
+        // 自定义日期格式
+        return arg.dayNumberText.replace("日","");
+
+      },
+      dateClick: function(info) {
+        alert('Clicked on: ' + info.dateStr);
+        const nextMonth = info.view.currentDate.add(1, 'month');
+
+        $('#calendar').fullCalendar('gotoDate', nextMonth);
+      }
     });
     calendar.render();
   });
 }
+
