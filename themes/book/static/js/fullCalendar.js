7070d0fda7248792f250793e35ae8fbeff9e47a7
2198764529 <2198764529@qq.com>
2023-12-06 18:53:29 +0800

    vault backup: 2023-12-06 18:53:29

--- themes/book/static/js/fullCalendar.js
+++ themes/book/static/js/fullCalendar.js
@@ -1,7 +1,7 @@
 function calendarRender(content) {
   document.addEventListener("DOMContentLoaded", function () {
     // let content = "{{ $page := .Site.GetPage `/posts/日志.md` }}{{ $page.RawContent }}";
-    const specificDateTime = new Date(2012, 11, 2, 18, 23); // 月份是从 0 开始的，所以 11 表示12月
+    const specificDateTime = new Date(); // 月份是从 0 开始的，所以 11 表示12月
     const year = specificDateTime.getFullYear();
     const month = String(specificDateTime.getMonth() + 1).padStart(2, '0'); // 月份加1
     const day = String(specificDateTime.getDate()).padStart(2, '0');
@@ -9,7 +9,6 @@ function calendarRender(content) {
     const minutes = String(specificDateTime.getMinutes()).padStart(2, '0');
     
     const dateTimeString = `⏰${year}年${month}月${day}日 ${hours}时${minutes}分`;
-    
     var calendarEl = document.getElementById("calendar");
     const jsonData = parseMarkdownToJSON(content);
     var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -20,8 +19,7 @@ function calendarRender(content) {
       events: jsonData,
       headerToolbar: {
         right: "", // 左侧按钮，包括前一个、后一个和今天
-        // center: 'title',        // 中间标题
-        left: "today",
+        left: "prev,today,next",
         // right: 'dayGridMonth,timeGridWeek,timeGridDay' // 右侧切换视图的按钮
        
 
@@ -32,20 +30,35 @@ function calendarRender(content) {
         week: '周视图', // 更改 "week" 按钮的标题
         day: '日视图' // 更改 "day" 按钮的标题
       },
-      titleFormat: { month: 'short', day: 'numeric' },
-      dayCellContent: function (arg) {
-        // 自定义日期格式
-        return arg.dayNumberText.replace("日","");
-
+      titleFormat:function (info) {
+        return `${info.date.year}年${info.date.month+1}月`;
       },
-      dateClick: function(info) {
-        alert('Clicked on: ' + info.dateStr);
-        const nextMonth = info.view.currentDate.add(1, 'month');
+      dayHeaderContent:function (info) {
+        return info.text.replace("周",'')
+      },
+      dayCellContent: function (info) {
+        // 自定义日期格式
+        var d = Lunar.fromDate(info.date);
+        var d2 = Solar.fromDate(info.date);
 
-        $('#calendar').fullCalendar('gotoDate', nextMonth);
-      }
+        var text=d.getFestivals()+d2.getFestivals();
+        if (text.length==0){
+          text = `<small>${d.getDayInChinese()}</small>`
+        }else{
+          text = `<small class="festivals">${text}</small>`
+        }
+        return {html:`<span>${info.dayNumberText.replace("日","")}</span> ${text}`}
+      },
+      // dateClick: function(info) {
+      //   calendar.gotoDate( info.dateStr );
+
+      // },
+     
     });
+    console.log(HolidayUtil.getHoliday(2020, 5, 2) + '');
+
     calendar.render();
   });
+  
 }
 
