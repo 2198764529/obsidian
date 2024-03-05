34ad3bffe70606fe2a68aebbc65f43432ff61454
2198764529 <2198764529@qq.com>
2024-03-05 16:37:50 +0800

    vault backup: 2024-03-05 16:37:50

--- themes/book/static/js/fullCalendar.js
+++ themes/book/static/js/fullCalendar.js
@@ -1,58 +1,70 @@
 function calendarRender() {
   document.addEventListener("DOMContentLoaded", function () {
-    // let content = "{{ $page := .Site.GetPage `/posts/日志.md` }}{{ $page.RawContent }}";
-    const specificDateTime = new Date(); // 月份是从 0 开始的，所以 11 表示12月
-    const year = specificDateTime.getFullYear();
-    const month = String(specificDateTime.getMonth() + 1).padStart(2, "0"); // 月份加1
-    const day = String(specificDateTime.getDate()).padStart(2, "0");
-    const hours = String(specificDateTime.getHours()).padStart(2, "0");
-    const minutes = String(specificDateTime.getMinutes()).padStart(2, "0");
-
-    const dateTimeString = `⏰${year}年${month}月${day}日 ${hours}时${minutes}分`;
-    var calendarEl = document.getElementById("calendar");
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      contentHeight: "auto",
-
-      initialView: "dayGridMonth",
-      locale: "zh-cn",
-      headerToolbar: {
-        right: "", // 左侧按钮，包括前一个、后一个和今天
-        left: "prev,today,next",
-        // right: 'dayGridMonth,timeGridWeek,timeGridDay' // 右侧切换视图的按钮
-      },
-      buttonText: {
-        today: dateTimeString, // 更改 "today" 按钮的标题
-        month: "月视图", // 更改 "month" 按钮的标题
-        week: "周视图", // 更改 "week" 按钮的标题
-        day: "日视图", // 更改 "day" 按钮的标题
-      },
-      titleFormat: function (info) {
-        return `${info.date.year}年${info.date.month + 1}月`;
-      },
-      dayHeaderContent: function (info) {
-        return info.text.replace("周", "");
-      },
-      dayCellContent: function (info) {
-        // 自定义日期格式
-        var d = Lunar.fromDate(info.date);
-        var d2 = Solar.fromDate(info.date);
-
-        var text = d.getFestivals() + d2.getFestivals();
-        if (text.length == 0) {
-          text = `<small>${d.getDayInChinese()}</small>`;
-        } else {
-          text = `<small class="festivals">${text}</small>`;
+    var events = [];
+    fetch("/data/myData.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        for (var i in data) {
+          var item = data[i];
+          for (var i2 in item["events"]) {
+            events.push({
+              title: item["events"][i2],
+              start: item["date"],
+            });
+          }
         }
-        return {
-          html: `<span>${info.dayNumberText.replace("日", "")}</span> ${text}`,
-        };
-      },
-      // dateClick: function(info) {
-      //   calendar.gotoDate( info.dateStr );
+        var calendarEl = document.getElementById("calendar");
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          contentHeight: "auto",
 
-      // },
-    });
-    calendar.render();
+          initialView: "dayGridMonth",
+          locale: "zh-cn",
+          headerToolbar: {
+            right: "prev,today,next", // 左侧按钮，包括前一个、后一个和今天
+            left: "title",
+            // right: 'dayGridMonth,timeGridWeek,timeGridDay' // 右侧切换视图的按钮
+          },
+          buttonText: {
+            month: "月视图", // 更改 "month" 按钮的标题
+            week: "周视图", // 更改 "week" 按钮的标题
+            day: "日视图", // 更改 "day" 按钮的标题
+          },
+          // titleFormat: function (info) {
+          //   return `${info.date.year}年${info.date.month + 1}月`;
+          // },
+          dayHeaderContent: function (info) {
+            return info.text.replace("周", "");
+          },
+          dayCellContent: function (info) {
+            // 添加节假日
+            var d = Lunar.fromDate(info.date);
+            var d2 = Solar.fromDate(info.date);
+
+            var text = d.getFestivals() + d2.getFestivals();
+            if (text.length == 0) {
+              text = `<small>${d.getDayInChinese()}</small>`;
+            } else {
+              text = `<small class="festivals">${text}</small>`;
+            }
+            return {
+              html: `<span>${info.dayNumberText.replace(
+                "日",
+                ""
+              )}</span> ${text}`,
+            };
+          },
+          events: events
+          // dateClick: function(info) {
+          //   calendar.gotoDate( info.dateStr );
+
+          // },
+        });
+        calendar.render();
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
   });
 }
 
